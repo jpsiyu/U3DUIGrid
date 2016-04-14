455a5d44c971316f9a74c10bc165de82fa457c6f
jpsiyu <jpsiyu@163.com>
2016-04-14 23:51:33 +0800

    add project

---
+++ Assets/Scripts/WrapContent.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WrapContent : MonoBehaviour
+{
+    public GameObject wrapGO;
+    UIWrapContent wrapContent;
+    List<int> numbers;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        numbers = new List<int>();
+        for(int i = 0; i<30; i++){
+            numbers.Add(i);
+        }
+        wrapContent = wrapGO.GetComponent<UIWrapContent>();
+        InitWrapContent();
+    }
+
+    private void InitWrapContent() {
+        wrapContent.onInitializeItem = OnUpdateItem;
+        wrapContent.minIndex = -(numbers.Count - 1);
+        wrapContent.maxIndex = 0;
+    }
+
+    private void OnUpdateItem(GameObject go, int index, int realIndex)
+    {
+        go.GetComponent<UILabel>().text = numbers[System.Math.Abs(realIndex)].ToString();
+    }
+}
+
